7aeaa1ddb6710f86e9881dfdc1b8bb730d147f82
aidajk77 <aidajkunic@gmail.com>
2025-09-09 10:49:11 +0200

    upload folder deleted

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,6 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-// Static files for uploaded images/logos
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 const imageRoutes = require('./routes/imageRoutes');
